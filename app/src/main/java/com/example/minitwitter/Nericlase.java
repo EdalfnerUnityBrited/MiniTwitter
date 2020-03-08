730e7606e4fae28496e73c8a979cc97bce77777c
elderdeveloper15 <54344130+elderdeveloper15@users.noreply.github.com>
2020-03-08 16:38:30 -0600

    agregado el pollito, ay papá

--- app/src/main/java/com/example/minitwitter/Nericlase.java
+++ app/src/main/java/com/example/minitwitter/Nericlase.java
@@ -3,4 +3,7 @@ package com.example.minitwitter;
 public class Nericlase {
 private  int pollitoconverduras;
 
+    public Nericlase(int pollitoconverduras) {
+        this.pollitoconverduras = pollitoconverduras;
+    }
 }
